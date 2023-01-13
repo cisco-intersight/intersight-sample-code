a262ba9177629ba5fd4d00f862ecb33188e09d7c
Ghufran Zahidi <18732053+Ghufz@users.noreply.github.com>
2023-01-13 11:59:48 +0530

    added powershell examples

--- profile/ucs_chassis_profile/powershell/ucs_chassis_profile.ps1
+++ profile/ucs_chassis_profile/powershell/ucs_chassis_profile.ps1
@@ -1,4 +1,13 @@
 # get the Organization ref, If otganization does not exist use the cmdlet New-IntersightOrganizationOrganization to create it.
 $orgRef = Get-IntersightOrganizationOrganization -Name "default" | Get-IntersightMoMoRef
 
-New-IntersightChassisProfile -Name "sample_chassis_profile_1" -Organization $orgRef
+$imcAccessRef = Get-IntersightAccessPolicy -Name "<Name of the Access policy>" | Get-IntersightMoMoRef
+
+$powerPolicyRef = Get-IntersightPowerPolicy -Name "<Name of the Power POlicy>" | Get-IntersightMoMoRef
+
+$snmpPolicyRef = Get-IntersightSnmpPolicy -Name "<Name of the snmp policy>" | Get-IntersightMoMoRef
+
+$thermalPolicyRef = Get-IntersightThermalPolicy -Name "<Name of the thermal policy>" | Get-IntersightMoMoRef
+
+New-IntersightChassisProfile -Name "sample_chassis_profile_1" -Organization $orgRef -PolicyBucket @($imcAccessRef,$powerPolicyRef,$snmpPolicyRef,$thermalPolicyRef) `
+        -Type Instance -TargetPlatform FIAttached
--- profile/ucs_domain_profile/powershell/ucs_domain_profile.ps1
+++ profile/ucs_domain_profile/powershell/ucs_domain_profile.ps1
@@ -1,1 +1,13 @@
-# New-IntersightFabricSwitchProfile 
+# Creating a cluster and siwtch profile without policies, policies can  be attached 
+
+$orgRef = Get-IntersightOrganizationOrganization -Name "<Org Name>" | Get-IntersightMoMoRef
+
+# create cluster switch policy
+
+$clusterSiwtchPolicy = New-IntersightFabricSwitchClusterProfile -Name "cluster_switch_profile_1" -Organization $orgRef
+
+$clusterProfileRef = $clusterSiwtchPolicy | Get-IntersightMoMoRef
+
+$fabric_A = New-IntersightFabricSwitchProfile -Name "Fabric_A_profile" -SwitchClusterProfile $clusterProfileRef
+
+$fabric_B = New-IntersightFabricSwitchProfile -Name "Fabric_B_profile" -SwitchClusterProfile $clusterProfileRef
