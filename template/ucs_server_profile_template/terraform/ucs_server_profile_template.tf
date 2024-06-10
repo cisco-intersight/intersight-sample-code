0613ebe03f924b2a1af71d6a3d90f185bd1f08a4
JINDAT BARADIA GIRISH. -X (jingiris - XORIANT CORPORATION at Cisco) <jingiris@cisco.com>
2024-06-10 11:22:46 +0530

    Pool examples added

--- template/ucs_server_profile_template/terraform/ucs_server_profile_template.tf
+++ template/ucs_server_profile_template/terraform/ucs_server_profile_template.tf
@@ -1,7 +1,7 @@
 provider "intersight" {
-  endpoint        = "https://intersight.com"
-  apikey          = "xxxxx27564612d30dxxxxx/5f21c9d97564612d30dd575a/5f9a8b877564612xxxxxxxx"
-  secretkey       = "C:\\secretKey.txt"
+  endpoint  = "https://intersight.com"
+  apikey    = "xxxxx27564612d30dxxxxx/5f21c9d97564612d30dd575a/5f9a8b877564612xxxxxxxx"
+  secretkey = "C:\\secretKey.txt"
 }
 
 resource "intersight_ntp_policy" "ntp_policy" {
@@ -14,12 +14,21 @@ resource "intersight_ntp_policy" "ntp_policy" {
   }
 }
 
-
 resource "intersight_kvm_policy" "kvm_policy" {
   name                     = "kvm_policy_11"
   enable_local_server_video = true
   enable_video_encryption   = true
   enabled                  = true
+  organization {
+    object_type = "organization.Organization"
+    moid        = var.organization
+  }
+}
+
+resource "intersight_bios_policy" "bios_policy" {
+  name                    = "bios_policy_11"
+  boot_option_retry       = "enabled"
+  intel_turbo_boost_tech  = "enabled"
   organization {
     object_type = "organization.Organization"
     moid        = var.organization
@@ -32,16 +41,24 @@ resource "intersight_server_profile_template" "server_profile_template" {
     object_type = "organization.Organization"
     moid        = var.organization
   }
+
   policy_bucket {
     moid        = intersight_ntp_policy.ntp_policy.moid
     object_type = "ntp.Policy"
     class_id    = "policy.AbstractPolicy"
     selector    = ""
   }
-  
+
   policy_bucket {
     moid        = intersight_kvm_policy.kvm_policy.moid
     object_type = "kvm.Policy"
+    class_id    = "policy.AbstractPolicy"
+    selector    = ""
+  }
+
+  policy_bucket {
+    moid        = intersight_bios_policy.bios_policy.moid
+    object_type = "bios.Policy"
     class_id    = "policy.AbstractPolicy"
     selector    = ""
   }
